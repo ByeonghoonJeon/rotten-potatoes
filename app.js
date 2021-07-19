37804b04e9db557ee2d92a6d023a94110896d901
Nathan Jeon <jeon4972@snu.ac.kr>
2021-07-19 21:39:38 +0700

    initial commit

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const app = express();
+
+const exphbs = require("express-handlebars");
+
+const mongoose = require("mongoose");
+mongoose.connect('mongodb://localhost/rotten-potatoes', { useNewUrlParser: true });
+const Review = mongoose.model('Review', {
+    title: String,
+    movieTitle: String
+  });
+let reviews = [
+    {title: "Great Review", movieTitle: "Batman II"},
+    {title: "Awesome Movie", movieTitle: "Titanic"}
+];
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+app.get('/', (req, res) => {
+    Review.find()
+      .then(reviews => {
+        res.render('reviews-index', { reviews: reviews });
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  })
+
+app.get("/reviews", function(req, res){
+    res.render("reviews-index", {reviews: reviews});
+});
+
+
+app.listen(3000, function(){
+    console.log("Server is working on port 3000");
+});
