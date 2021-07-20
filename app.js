9b0096270a84a2dc1d5505441eb3b27d645dc14d
Nathan Jeon <jeon4972@snu.ac.kr>
2021-07-20 19:25:50 +0700

    Practice

--- app.js
+++ app.js
@@ -5,8 +5,9 @@ app.use(express.urlencoded()); //Parse URL-encoded bodies
 
 const exphbs = require("express-handlebars");
 
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/rotten-potatoes', { useNewUrlParser: true });
+
 const Review = mongoose.model('Review', {
     title: String,
     description: String,
@@ -14,44 +15,55 @@ const Review = mongoose.model('Review', {
     rating: Number
   });
 let reviews = [
-    {title: "Great Review", movieTitle: "Batman II"},
-    {title: "Awesome Movie", movieTitle: "Titanic"}
+    {title: "Great Review", description: "Good", movieTitle: "Batman II", rating: 4},
+    {title: "Awesome Movie", description: "Nice", movieTitle: "Titanic", rating: 5}
 ];
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
 app.get('/', (req, res) => {
-    Review.find()
-      .then(reviews => {
-        res.render('reviews-index', { reviews: reviews });
-      })
-      .catch(err => {
-        console.log(err);
-      })
+  Review.find()
+    .then(reviews => {
+      res.render('reviews-index', { reviews: reviews });
+    })
+    .catch(err => {
+      console.log(err);
   })
+})
+
+
+app.get('/reviews', (req, res) => {
+  res.render('reviews-index', { reviews: reviews });
+})
+
 
 app.get("/reviews/new", function(req, res){
   res.render("reviews-new",{})
-})
-
-app.get("/reviews/:id", function(req, res){
-  Review.findById(req.params.id).then(function(review){
-    res.render("reviews-show", {review:review})
-  }).catch(function(err){
-    console.log(err.message);
-  });
 });
 
-app.post("/reviews", function(req, res){
-  Review.create(req.body).then(function(review){
-    console.log(review)
-    res.redirect('/reviews/${review.id}')
-  }).catch(function(err){
+app.get('/reviews/:id', (req, res) => {
+  Review.findById(req.params.id).then((review) => {
+    res.render('reviews-show', { review: review })
+  }).catch((err) => {
     console.log(err.message);
   })
 })
 
+app.post('/reviews', (req, res) => {
+  Review.create(req.body).then((review) => {
+    console.log(review)
+    res.redirect(`/reviews/${review._id}`) // Redirect to reviews/:id
+  }).catch((err) => {
+    console.log(err.message)
+  })
+})
+
+app.get('/reviews/:id/edit', (req, res) => {
+  Review.findById(req.params.id, function(err, review) {
+    res.render('reviews-edit', {review: review});
+  })
+})
   
 
 app.listen(3000, function(){
