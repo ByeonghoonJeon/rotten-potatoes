679a48608b0c90da2d5e5e45dc2fa2a6464fbe51
Nathan Jeon <jeon4972@snu.ac.kr>
2021-07-20 20:44:51 +0700

    Users can make reviews

--- app.js
+++ app.js
@@ -3,15 +3,21 @@ const app = express();
 const exphbs = require("express-handlebars");
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost/rotten-potatoes", {useNewUrlParser: true});
+app.use(express.json()); //Used to parse JSON bodies
+app.use(express.urlencoded()); //Parse URL-encoded bodies
 
-const Review = mongoose.model("Review", {
-    title: String,
-    movieTitle: String
+mongoose.connect("mongodb://localhost/rotten-potatoes", {
+  useNewUrlParser: true,
 });
 
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
-app.set('view engine', 'handlebars');
+const Review = mongoose.model("Review", {
+  title: String,
+  movieTitle: String,
+  description: String
+});
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
 // OUR MOCK ARRAY OF PROJECTS
 // let reviews = [
@@ -20,20 +26,35 @@ app.set('view engine', 'handlebars');
 //     { title: "Fun Movie", movieTitle: "LALA Land"}
 //   ]
 
-  app.get('/', (req, res) => {
-    Review.find()
-      .then(reviews => {
-        res.render('reviews-index', { reviews: reviews });
-      })
-      .catch(err => {
-        console.log(err);
-      })
-  })
+app.get("/", (req, res) => {
+  Review.find()
+    .then((reviews) => {
+      res.render("reviews-index", { reviews: reviews });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
-  // INDEX
-app.get('/reviews', (req, res) => {
-    res.render('reviews-index', { reviews: reviews });
-  })
+// INDEX
+app.get("/reviews", (req, res) => {
+  res.render("reviews-index", { reviews: reviews });
+});
+
+app.get("/reviews/new", (req, res) => {
+  res.render("reviews-new", {});
+});
+
+app.post("/reviews", (req, res) => {
+  Review.create(req.body)
+    .then((review) => {
+      console.log(review);
+      res.redirect("/");
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+});
 
 app.listen(3000, () => {
   console.log("App listening on port 3000!");
