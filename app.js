b45cedc4b8344a342a02dffc8aac9e5e50ecdf99
Nathan Jeon <jeon4972@snu.ac.kr>
2021-07-20 19:55:41 +0700

    Users can see reviews

--- app.js
+++ app.js
@@ -5,8 +5,19 @@ const exphbs = require("express-handlebars");
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
-app.get('/', (req, res) => {
-    res.render('home', { msg: 'Handlebars are Cool!' });
+// OUR MOCK ARRAY OF PROJECTS
+let reviews = [
+    { title: "Great Review", movieTitle: "Batman II" },
+    { title: "Awesome Movie", movieTitle: "Titanic" }
+  ]
+
+  app.get('/', (req, res) => {
+    res.render('reviews-index', { reviews: reviews });
+  })
+
+  // INDEX
+app.get('/reviews', (req, res) => {
+    res.render('reviews-index', { reviews: reviews });
   })
 
 app.listen(3000, () => {
