8b20c1a1f098037c1e1c46fe79840927bf11617f
Nathan Jeon <jeon4972@snu.ac.kr>
2021-07-21 23:23:43 +0700

    Problem solved

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const methodOverride = require("method-override");
 const app = express();
 const exphbs = require("express-handlebars");
 const mongoose = require("mongoose");
-const reviews = require('./controllers/reviews')(app);
 
 // override with POST having ?_method=DELETE or ?_method=PUT
 app.use(methodOverride("_method"));
@@ -15,15 +14,10 @@ mongoose.connect("mongodb://localhost/rotten-potatoes", {
   useNewUrlParser: true,
 });
 
-const Review = mongoose.model("Review", {
-  title: String,
-  movieTitle: String,
-  rating: Number,
-  description: String,
-});
-
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
+
+const reviews = require('./controllers/reviews')(app);
 
 // OUR MOCK ARRAY OF PROJECTS
 // let reviews = [
@@ -31,78 +25,6 @@ app.set("view engine", "handlebars");
 //     { title: "Awesome Movie", movieTitle: "Titanic" },
 //     { title: "Fun Movie", movieTitle: "LALA Land"}
 //   ]
-
-app.get("/", (req, res) => {
-  Review.find().lean()
-    .then((reviews) => {
-      res.render("reviews-index", { reviews: reviews });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-
-// INDEX
-app.get("/reviews", (req, res) => {
-  res.render("reviews-index", { reviews: reviews });
-});
-
-// NEW
-app.get("/reviews/new", (req, res) => {
-  res.render("reviews-new", { title: "New Review" });
-});
-
-// SHOW
-app.get("/reviews/:id", (req, res) => {
-  Review.findById(req.params.id).lean()
-    .then((review) => {
-        console.log(review);
-      res.render("reviews-show", { review: review });
-    })
-    .catch((err) => {
-      console.log(err.message);
-    });
-});
-
-// EDIT
-app.get("/reviews/:id/edit", (req, res) => {
-  Review.findById(req.params.id, function (err, review) {
-    res.render("reviews-edit", { review: review, title: "Edit Review" });
-  });
-});
-
-// UPDATE
-app.put("/reviews/:id", (req, res) => {
-  Review.findByIdAndUpdate(req.params.id, req.body)
-    .then((review) => {
-      res.redirect(`/reviews/${review._id}`);
-    })
-    .catch((err) => {
-      console.log(err.message);
-    });
-});
-
-// CREATE
-app.post("/reviews", (req, res) => {
-  Review.create(req.body)
-    .then((review) => {
-      console.log(review);
-      res.redirect(`/reviews/${review._id}`); // Redirect to reviews/:id
-    })
-    .catch((err) => {
-      console.log(err.message);
-    });
-});
-
-// DELETE
-app.delete('/reviews/:id', function (req, res) {
-  console.log("DELETE review")
-  Review.findByIdAndRemove(req.params.id).then((review) => {
-    res.redirect('/');
-  }).catch((err) => {
-    console.log(err.message);
-  })
-})
 
 app.listen(3000, () => {
   console.log("App listening on port 3000!");
--- controllers/reviews.js
+++ controllers/reviews.js
@@ -1,7 +1,7 @@
 const Review = require('../models/review');
 module.exports = function(app) {
     app.get('/', (req, res) => {
-      Review.find()
+      Review.find().lean()
         .then(reviews => {
           res.render('reviews-index', {reviews: reviews});
         })
@@ -9,4 +9,65 @@ module.exports = function(app) {
           console.log(err);
         });
     });
+    // INDEX
+app.get("/reviews", (req, res) => {
+  res.render("reviews-index", { reviews: reviews });
+});
+
+// NEW
+app.get("/reviews/new", (req, res) => {
+  res.render("reviews-new", { title: "New Review" });
+});
+
+// SHOW
+app.get("/reviews/:id", (req, res) => {
+  Review.findById(req.params.id).lean()
+    .then((review) => {
+        console.log(review);
+      res.render("reviews-show", { review: review });
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+});
+
+// EDIT
+app.get("/reviews/:id/edit", (req, res) => {
+  Review.findById(req.params.id, function (err, review) {
+    res.render("reviews-edit", { review: review, title: "Edit Review" });
+  });
+});
+
+// UPDATE
+app.put("/reviews/:id", (req, res) => {
+  Review.findByIdAndUpdate(req.params.id, req.body)
+    .then((review) => {
+      res.redirect(`/reviews/${review._id}`);
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+});
+
+// CREATE
+app.post("/reviews", (req, res) => {
+  Review.create(req.body)
+    .then((review) => {
+      console.log(review);
+      res.redirect(`/reviews/${review._id}`); // Redirect to reviews/:id
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+});
+
+// DELETE
+app.delete('/reviews/:id', function (req, res) {
+  console.log("DELETE review")
+  Review.findByIdAndRemove(req.params.id).then((review) => {
+    res.redirect('/');
+  }).catch((err) => {
+    console.log(err.message);
+  })
+})
   }
