3af29e98c5743b69dd037b912032e2291f2cca9e
Nathan Jeon <jeon4972@snu.ac.kr>
2021-07-22 19:12:34 +0700

    Bootstrap

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -1,73 +1,87 @@
-const Review = require('../models/review');
-module.exports = function(app) {
-    app.get('/', (req, res) => {
-      Review.find().lean()
-        .then(reviews => {
-          res.render('reviews-index', {reviews: reviews});
-        })
-        .catch(err => {
-          console.log(err);
-        });
-    });
-    // INDEX
-app.get("/reviews", (req, res) => {
-  res.render("reviews-index", { reviews: reviews });
-});
+const Review = require("../models/review");
 
-// NEW
-app.get("/reviews/new", (req, res) => {
-  res.render("reviews-new", { title: "New Review" });
-});
+module.exports = function (app) {
+  app.get("/", (req, res) => {
+    Review.find()
+      .lean()
+      .then((reviews) => {
+        res.render("reviews-index", { reviews: reviews });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
+  // INDEX
+  app.get("/reviews", (req, res) => {
+    Review.find()
+      .lean()
+      .then((reviews) => {
+        res.render("reviews-index", { reviews: reviews });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
 
-// SHOW
-app.get("/reviews/:id", (req, res) => {
-  Review.findById(req.params.id).lean()
-    .then((review) => {
+  // NEW
+  app.get("/reviews/new", (req, res) => {
+    res.render("reviews-new", { title: "New Review" });
+  });
+
+  // SHOW
+  app.get("/reviews/:id", (req, res) => {
+    Review.findById(req.params.id)
+      .lean()
+      .then((review) => {
         console.log(review);
-      res.render("reviews-show", { review: review });
-    })
-    .catch((err) => {
-      console.log(err.message);
-    });
-});
+        res.render("reviews-show", { review: review });
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  });
 
-// EDIT
-app.get("/reviews/:id/edit", (req, res) => {
-  Review.findById(req.params.id, function (err, review) {
-    res.render("reviews-edit", { review: review, title: "Edit Review" });
-  }).lean();
-});
+  // EDIT
+  app.get("/reviews/:id/edit", (req, res) => {
+    Review.findById(req.params.id, function (err, review) {
+      res.render("reviews-edit", { review: review, title: "Edit Review" });
+    }).lean();
+  });
 
-// UPDATE
-app.put("/reviews/:id", (req, res) => {
-  Review.findByIdAndUpdate(req.params.id, req.body).lean()
-    .then((review) => {
-      res.redirect(`/reviews/${review._id}`);
-    })
-    .catch((err) => {
-      console.log(err.message);
-    });
-});
+  // UPDATE
+  app.put("/reviews/:id", (req, res) => {
+    Review.findByIdAndUpdate(req.params.id, req.body)
+      .lean()
+      .then((review) => {
+        res.redirect(`/reviews/${review._id}`);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  });
 
-// CREATE
-app.post("/reviews", (req, res) => {
-  Review.create(req.body)
-    .then((review) => {
-      console.log(review);
-      res.redirect(`/reviews/${review._id}`); // Redirect to reviews/:id
-    })
-    .catch((err) => {
-      console.log(err.message);
-    });
-});
+  // CREATE
+  app.post("/reviews", (req, res) => {
+    Review.create(req.body)
+      .then((review) => {
+        console.log(review);
+        res.redirect(`/reviews/${review._id}`); // Redirect to reviews/:id
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  });
 
-// DELETE
-app.delete('/reviews/:id', function (req, res) {
-  console.log("DELETE review")
-  Review.findByIdAndRemove(req.params.id).lean().then((review) => {
-    res.redirect('/');
-  }).catch((err) => {
-    console.log(err.message);
-  })
-})
-  }
+  // DELETE
+  app.delete("/reviews/:id", function (req, res) {
+    console.log("DELETE review");
+    Review.findByIdAndRemove(req.params.id)
+      .lean()
+      .then((review) => {
+        res.redirect("/");
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  });
+};
--- models/review.js
+++ models/review.js
@@ -5,7 +5,9 @@ const mongoose = require('mongoose');
 const Review = mongoose.model('Review', {
   title: String,
   description: String,
-  movieTitle: String
+  movieTitle: String,
+  rating: String
+  
 });
 
 module.exports = Review;
