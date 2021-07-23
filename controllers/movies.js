9c39b0355d39ad7dcc9a51264bb071396cc061af
Nathan Jeon <jeon4972@snu.ac.kr>
2021-07-23 21:15:20 +0700

    Practice

--- controllers/movies.js
+++ controllers/movies.js
@@ -1,12 +1,27 @@
 module.exports = function (app) {
+  const { MovieDb } = require("moviedb-promise");
+  const moviedb = new MovieDb("");
 
-    const { MovieDb } = require('moviedb-promise')
-    const moviedb = new MovieDb('')
+  app.get("/", (req, res) => {
+    moviedb
+      .miscNowPlayingMovies()
+      .then((response) => {
+        res.render("movies-index", { movies: response.results });
+      })
+      .catch(console.error);
+  });
 
-    app.get('/', (req, res) => {
-        moviedb.miscNowPlayingMovies().then(response => {
-            res.render('movies-index', { movies: response.results });
-        }).catch(console.error)
-    })
+  app.get("/movies/:id", (req, res) => {
+    moviedb
+      .movieInfo({ id: req.params.id })
+      .then((movie) => {
+        //   moviedb.Videos({ id: req.params.id }).then(videos => {
+        //     movie.trailer_youtube_id = videos.youtube[0].source;
+        //     console.log('VIDEOS.TRAILER_YOUTUBE_ID', movie.trailer_youtube_id);
 
-}
+        res.render("movies-show", { movie: movie });
+        //   }).catch(console.error);
+      })
+      .catch(console.error);
+  });
+};
